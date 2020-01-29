1dbbc67ad9a7ad262bbe38834014c6a9846f14a0
swhiteinventor <swhiteinventor@gmail.com>
2020-01-29 18:11:59 -0500

    got one video source to pop up

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QCameraInfo>
+#include <QCameraViewfinder>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QCamera* camera = new QCamera;
+    QCameraViewfinder* viewfinder = new QCameraViewfinder();
+    viewfinder->show();
+    camera->setViewfinder(viewfinder);
+    camera->setCaptureMode(QCamera::CaptureStillImage);
+    camera->start();
 }
 
 MainWindow::~MainWindow()
