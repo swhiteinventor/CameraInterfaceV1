7b3678fc9c1c78c6220527a278a0700b02516bd5
swhiteinventor <swhiteinventor@gmail.com>
2020-02-05 11:52:26 -0500

    camera switching is binded to a button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <QCameraInfo>
 #include <QCameraViewfinder>
 
+int switchCounter = 1;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -38,4 +40,25 @@ bool MainWindow::checkCameraAvailability()
         return true;
     else
         return false;
+}
+
+void MainWindow::on_Manual_SwitchButton_clicked()
+{
+    //figure out which widget should be made larger or smaller
+    QStackedWidget* makeLarger = ui->stackedWidgetHead;
+    QStackedWidget* makeSmaller = ui->stackedWidgetArm;
+    if (switchCounter%2 == 1)
+    {
+        makeLarger = ui->stackedWidgetArm;
+        makeSmaller = ui->stackedWidgetHead;
+    }
+
+    makeLarger->move(30,80);
+    makeLarger->resize(900,675);
+    makeSmaller->resize(180,135);
+    makeSmaller->move(750,80);
+    makeSmaller->raise();
+
+    //increment the counter
+    switchCounter ++;
 }
